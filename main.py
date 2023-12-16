05dffa63fa3fbc5fc07b44d22f7a659268e42c1e
Ivan Pants <vania.pants@yandex.ru>
2023-12-16 14:24:37 +0300

    add screen shot mod

---
+++ main.py
@@ -0,0 +1,18 @@
+import easyocr
+
+
+def text_recognition(file_path):
+    reader = easyocr.Reader(["ru", "en"])
+    result =reader.readtext(file_path, detail=0)
+
+    return result
+
+
+def main():
+    file_path = r'assets\new.png'
+    print(text_recognition(file_path=file_path))
+
+
+if __name__ == "__main__":
+    main()
+
